57c42321230deb4f9acb0ca24a37dc94d31b42a5
rohithna98 <53929809+rohithna98@users.noreply.github.com>
2019-08-09 12:23:59 +0530

    Create rohith.c

---
+++ rohith.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    float x,p;
+    int sum;
+    printf("enter cofee amount and discount");
+    scanf("%f %f",&x,&p);
+    sum=0;
+    for(i=0;i>100;i++)
+    {
+      sum=(x/p*100)
+    }
+    printf("%d",sum);
+    printf("/n");
+    return 0;
+}
